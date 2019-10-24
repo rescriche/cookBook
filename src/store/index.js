aa25aed7bd9a6124404ac5153e19e0be1002bfc2
Raquel Escriche <raquel.escriche@denkwerk.com>
2019-10-24 15:47:18 +0200

    add redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,8 @@ import { createStore } from 'redux';
 import rootReducer from './rootReducer';
 
 const store = createStore(
-    rootReducer
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
